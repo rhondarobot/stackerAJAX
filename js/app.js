8dd522ef48d135cbc05853c88e0f63a4975f20bc
rhondarobot <rrachellej@gmail.com>
2016-09-06 20:30:11 -0700

    initial push

--- js/app.js
+++ js/app.js
@@ -91,4 +91,95 @@ $(document).ready( function() {
 		var tags = $(this).find("input[name='tags']").val();
 		getUnanswered(tags);
 	});
-});
+}); //end of unanswered questions
+
+/*---start of showTopAnswerers section 2---*/
+//create a separate object for Top Answerers (var showAnswers)
+//type in a keyword in View the Top Answerers for a Tag
+var showTopAnswerers = function(answerer) {
+	console.log(answerer);
+	// clone our result template code
+	var result = $('.templates .answerer').clone();
+	
+	// set the answerer properties in result
+	var answererElem = result.find('.top-answerer a');
+	answererElem.attr('href', answerer.link);
+	answererElem.text(answerer.user.display_name);
+
+	// set the reputation properties in result
+	var reputationElem = result.find('.reputation');
+	reputation.text(answerer.reputation);
+
+	// set the score properties in result
+	var scoreElem = result.find('.score');
+	score.text(answerer.score);
+
+	// set user link properties in result
+	var answererLink = result.find('.answerer-link');
+	answerer-link.html('<p>Link to Answerer Profile: <a target="_blank" '+
+		'href=http://stackoverflow.com/users/' + answerer.user.link + '>' +
+		'</a></p>' 
+	);
+
+	return result;
+	console.log(result);
+};
+
+// this function takes the results object from StackOverflow
+// and returns the number of results and tags to be appended to DOM
+var showAnswererResults = function(query, resultNum) {
+	var results = resultNum + ' results for <strong>' + query + '</strong>';
+	return results;
+};
+
+// takes error string and turns it into displayable DOM element
+var showError = function(error){
+	var errorElem = $('.templates .error').clone();
+	var errorText = '<p>' + error + '</p>';
+	errorElem.append(errorText);
+};
+
+// takes a string of semi-colon separated tags to be searched
+// for on StackOverflow
+var getAnswerers = function(tags) {
+	
+	// the parameters we need to pass in our request to StackOverflow's API
+	var request = { 
+		site: 'stackoverflow',
+	};
+	
+	$.ajax({
+		url: "http://api.stackexchange.com/2.2/' + tags +'/tag/top-answerers/all_time",
+		data: request,
+		dataType: "jsonp",//use jsonp to avoid cross origin issues
+		type: "GET",
+	})
+	.done(function(result){ //this waits for the ajax to return with a succesful promise object
+		var answererResults = showAnswererResults(result.items.length);
+
+		$('.search-results').html(answererResults);
+		//$.each is a higher order function. It takes an array and a function as an argument.
+		//The function is executed once for each item in the array.
+		$.each(result.items, function(i, item) {
+			var question = showQuestion(item);
+			$('.answerer').append(question);
+		});
+	})
+	.fail(function(jqXHR, error){ //this waits for the ajax to return with an error promise object
+		var errorElem = showError(error);
+		$('.answerer-results').append(errorElem);
+	});
+};
+
+
+$(document).ready( function() {
+	$('.inspiration-getter').submit( function(e){
+		e.preventDefault();
+		// zero out results if previous search has run
+		$('.answerer-results').html('');
+		// get the value of the tags the user submitted
+		var tags = $(this).find("input[name='tags']").val();
+		getAnswerers(tags);
+	});
+}); //end of showTopAnswerers section 2
+
